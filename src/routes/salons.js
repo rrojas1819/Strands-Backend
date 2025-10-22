fa441224a58d355b11e4cf5497c00b7cc610c36d
Mitchell Lipnitsky <jediminor751@gmail.com>
2025-10-21 20:43:12 -0400

    Fix: UAR-1.3/1.4 added role authentification

--- src/routes/salons.js
+++ src/routes/salons.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const salonController = require('../controllers/salonController');
-const {authenticateToken} = require('../middleware/auth.middleware');
+const {authenticateToken, roleAuthorization} = require('../middleware/auth.middleware');
 
 
-router.post('/check', authenticateToken, salonController.checkOwnerHasSalon);
-router.post('/create', authenticateToken, salonController.createSalon);
+router.post('/check', authenticateToken, roleAuthorization(['OWNER']), salonController.checkOwnerHasSalon);
+router.post('/create', authenticateToken, roleAuthorization(['OWNER']), salonController.createSalon);
 
 
 
